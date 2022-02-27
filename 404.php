e155ec3d9c54f583b0134b252b1f2ac36bd29680
andresguti19fc <chavogutierrez19@gmail.com>
2022-02-27 13:37:00 -0300

    agg sitemap

--- 404.php
+++ 404.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Error</title>
 </head>
-<body>
-    <h1>Error 404</h1>
-    <p>lo siento, esta poagina no esta disponible</p>
+<body class="container404">
+    <h1 class="h1404">Error 404</h1>
+    <p class="p404">lo siento, esta poagina no esta disponible</p>
     <a href="index.html">ir a la pagina web</a>
 </body>
 </html>
